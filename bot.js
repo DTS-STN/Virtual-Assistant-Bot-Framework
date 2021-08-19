87bf6e65b1baaedd89ceb981c534642c4f4e4f88
Benoit Cloutier <benoit.bc.cloutier@gmail.com>
2021-08-19 15:51:30 -0400

    fixed lint.

--- bot.js
+++ bot.js
@@ -5,8 +5,8 @@ const { ActivityHandler, MessageFactory } = require("botbuilder");
 
 class EchoBot extends ActivityHandler {
   constructor() {
-            super();
-            let veb = 1;
+    super();
+    let veb = 1;
     // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
     this.onMessage(async (context, next) => {
       const replyText = `Echo: ${context.activity.text}`;
